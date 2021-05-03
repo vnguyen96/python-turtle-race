055c20687ed4dd6274a2b9ea4f7e5f0809b5b384
Vinh <vinhnguyen0096@gmail.com>
2021-05-03 13:11:29 -0700

    Finished turtle race project

--- TurtR.py
+++ TurtR.py
@@ -1,3 +1,11 @@
+import turtle
+import time 
+import random 
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 
+'pink', 'brown', 'cyan']
+
 def get_number_of_racers():
     racers = 0 
     while True:
@@ -13,5 +21,49 @@ def get_number_of_racers():
         else:
             print('Number not in range 2-10. Try again!')
 
+
+
+
+def race(colors):
+    turtles = create_turtles(colors)
+    
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+            
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10: 
+                return colors[turtles.index(turtle)]
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer. left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+        
+    return turtles
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racing!")
+
 racers = get_number_of_racers()
-print(racers)
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]  #what this does is basically picking number of racers
+
+
+winner = race(colors)
+print("The winner is the turtle with color:", winner)
+time.sleep(5)
+
