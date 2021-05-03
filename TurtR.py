c146ef7a7d30f55860d0ad4642c1530edeed9cda
Vinh <vinhnguyen0096@gmail.com>
2021-05-02 20:44:29 -0700

    Adding first function

---
+++ TurtR.py
@@ -0,0 +1,17 @@
+def get_number_of_racers():
+    racers = 0 
+    while True:
+        racers = input('Enter the number of racers (2-10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not numeric... Try Again!')
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in range 2-10. Try again!')
+
+racers = get_number_of_racers()
+print(racers)
