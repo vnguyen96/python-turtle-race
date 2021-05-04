949d145a564dee2f46b9fc4612a9876baaf0386a
Vinh <vinhnguyen0096@gmail.com>
2021-05-03 21:04:23 -0700

    Changing order

--- TurtR.py
+++ TurtR.py
@@ -2,9 +2,6 @@ import turtle
 import time 
 import random 
 
-WIDTH, HEIGHT = 500, 500
-COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 
-'pink', 'brown', 'cyan']
 
 def get_number_of_racers():
     racers = 0 
@@ -21,8 +18,39 @@ def get_number_of_racers():
         else:
             print('Number not in range 2-10. Try again!')
 
+racers = get_number_of_racers()  ##call get_number_of_racers function
 
 
+WIDTH, HEIGHT = 500, 500 
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racing!")
+
+init_turtle()
+
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 
+'pink', 'brown', 'cyan']
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]  #what this does is basically picking number of racers
+
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+        
+    return turtles
 
 def race(colors):
     turtles = create_turtles(colors)
@@ -36,31 +64,7 @@ def race(colors):
             if y >= HEIGHT // 2 - 10: 
                 return colors[turtles.index(turtle)]
 
-def create_turtles(colors):
-    turtles = []
-    spacingx = WIDTH // (len(colors) + 1)
-    for i, color in enumerate(colors):
-        racer = turtle.Turtle()
-        racer.color(color)
-        racer.shape('turtle')
-        racer. left(90)
-        racer.penup()
-        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
-        racer.pendown()
-        turtles.append(racer)
-        
-    return turtles
 
-def init_turtle():
-    screen = turtle.Screen()
-    screen.setup(WIDTH, HEIGHT)
-    screen.title("Turtle Racing!")
-
-racers = get_number_of_racers()
-init_turtle()
-
-random.shuffle(COLORS)
-colors = COLORS[:racers]  #what this does is basically picking number of racers
 
 
 winner = race(colors)
